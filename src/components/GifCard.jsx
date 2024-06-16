7981761f275bd5f1b78c2bcd81090f96c2c5eb3e
yboffill91 <yboffill91@gmail.com>
2024-06-16 10:30:57 -0400

    Revisar problemas en cantidad de peticiones a api

--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -1,6 +1,19 @@
 import { PropTypes } from "prop-types";
 
+const fetchGif = async (gif) => {
+  const request = await fetch(
+    `https://api.giphy.com/v1/gifs/search/?api_key=fw44TykIaBxpG03ZW8rn5FrgnBGKYUYm&q=${gif}&limit=1`
+  );
+  const { data } = await request.json();
+  console.log(data);
+  return {
+    id: data.id,
+    title: data.title,
+    gifUrl: data.images.downsized_still.url,
+  };
+};
 export const GifCard = ({ key, category }) => {
+  fetchGif(category);
   return (
     <div className="card col-md-3" key={key}>
       <img src="../../public/uno.jpg" className="card-img-top" alt={category} />
